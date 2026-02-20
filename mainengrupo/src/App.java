b5571d5834bbe5a8ddeba3a8eaacdf9fd37e0433
alejandro-CIDE <ahalcon@nigul.cide.es>
2026-02-20 14:41:45 +0100

    Mostrar resultado

--- mainengrupo/src/App.java
+++ mainengrupo/src/App.java
@@ -2,10 +2,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Nombre del alumno: ");
+        String nombreAl = sc.next();
 
+        afegirnota alumno = new afegirnota(nombreAl);
 
+        for (int i=0;i<3;i++) {
+            System.out.println("Nota "+(i+1)+": ");
+            double nota = sc.nextDouble();
+            alumno.setNumNotas(i, nota);
+        }
+
+       mostrarresultat resultado = new mostrarresultat(alumno);
 
     }
 }
--- mainengrupo/src/calcularmitjana.java
+++ mainengrupo/src/calcularmitjana.java
@@ -1,13 +1,15 @@
 public class calcularmitjana {
+    
     public static double calcularMedia(afegirnota alumno) {
-        int[] notas = alumno.getNumNotas();
-        int total = 0;
+
+        double[] notas = alumno.getNumNotas();
+        double total = 0;
 
         for (int i = 0; i < notas.length; i++) {
             total += notas[i];
         }
 
-        return (double)total/notas.length;
+        return total/(double)notas.length;
     }
 
 }
